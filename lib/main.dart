a54cb02c58bcbda0037840d7bc77fe4a651c331e
Fadiil <fadilthoriq201@gmail.com>
2022-10-13 07:30:45 +0700

    testing-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('First-App-Thoriq'),
-          centerTitle: true,
+          title: Text("Welcome"),
         ),
-        // ignore: prefer_const_constructors
-        body: Center(child: Text("Hallo world!")),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-        ),
+        body: Center(
+            child: Container(
+                color: Colors.blue,
+                width: 150,
+                height: 50,
+                child: Text("Hello World"))),
       ),
-    ));
+    );
+  }
+}
+
+
+
+
+// void main() => runApp(MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('First-App-Thoriq'),
+//           centerTitle: true,
+//         ),
+//         // ignore: prefer_const_constructors
+//         body: Center(child: Text("Hallo world!")),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {},
+//           child: Text('click'),
+//         ),
+//       ),
+//     ));
